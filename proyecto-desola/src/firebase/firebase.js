77c3bb1b0bf01cc607672140f01f9132d6d3e97b
Andreina De Sola <andreinadesola@gmail.com>
2023-03-01 20:06:15 -0300

    firebase y img reducidas

---
+++ proyecto-desola/src/firebase/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdBnMvTcSiRTpBEk-Iysq-LbVmyrUmVPg",
+  authDomain: "proyecto-react-75934.firebaseapp.com",
+  projectId: "proyecto-react-75934",
+  storageBucket: "proyecto-react-75934.appspot.com",
+  messagingSenderId: "350681035198",
+  appId: "1:350681035198:web:b3e9dbeb9c2b714f84c9ef"
+};
+
+const app = initializeApp(firebaseConfig);
