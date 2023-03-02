1e486d059d28c63b54c5ab4969915b44b7441b10
Andreina De Sola <andreinadesola@gmail.com>
2023-03-02 17:06:07 -0300

    fix

--- proyecto-desola/src/context/CarritoContext.js
+++ proyecto-desola/src/context/CarritoContext.js
@@ -1,21 +1,39 @@
-import { useContext, createContext, UseState } from "react";
+import { useContext, createContext, useState } from "react";
 
-const CarritoContext = createContext();
+const CarritoContext = createContext()
 
-export const useCarritoContext = () => useContext(CarritoContext);
+export const useCarritoContext = () => useContext(CarritoContext)
 
 export const CarritoProvider = (props) => {
+
     const [carrito, setCarrito] = useState([])
 
     //Si existe producto en el carrito
     const isInCart = (id) => {
-        //Existe lo devuelve, si no undefined
+    
         return carrito.find(prod => prod.id === id)
     }
 
+    //Agregar producto
+    const addItem = (producto, cantidad) => {
+        if(isInCart(producto.id)) { 
+            const indice = carrito.findIndex(prod => prod.id === producto.id)
+            const aux = [...carrito]
+            aux[indice].cant = cantidad
+            setCarrito(aux)
+        } else { 
+            const prodCart ={
+                ...producto, 
+                cant: cantidad
+            }
+           
+            setCarrito([...carrito, prodCart])
+
+        }
+    }
     //Vaciar carrito
     const emptyCart = () => {
-        setCarrito([]);
+        setCarrito([])
     }
 
     //Eliminar producto
@@ -32,28 +50,11 @@ export const CarritoProvider = (props) => {
     const totalPrice = () => {
         return carrito.reduce((acum, prod) => acum += (prod.cant * prod.precio), 0)
     }
-
-
-    //Agregar producto
-    const addItem = (producto, cantidad) => {
-        if (isInCart(producto.id)) {
-            const indice = carrito.findIndex(prod => prod.id === producto.id)
-            const aux = [...carrito]
-            aux[indice].cant = cantidad
-            setCarrito(aux)
-        }
-        else {
-            const prodCart = {
-                ...producto,
-                cant: cantidad
-            }
-            setCarrito([...carrito, prodCart])
-        }
-    }
     console.log(carrito)
+
     return (
-        <CarritoContext.Provider value={{ carrito, addItem, removeItem, emptyCart, getItemQuantity, totalPrice }}>
-            {props.childrean}
+        <CarritoContext.Provider value={{carrito, addItem, removeItem, emptyCart, getItemQuantity, totalPrice}}>
+                {props.children}
         </CarritoContext.Provider>
     )
 
